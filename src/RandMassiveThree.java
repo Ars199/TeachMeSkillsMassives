bad98008e742b1e0dde262ebd5250ed59693b13e
Arseniy Yanson <arseniy.yanson@realizeideas.net>
2020-09-07 17:11:04 +0300

    DZ3

--- src/RandMassiveThree.java
+++ src/RandMassiveThree.java
@@ -2,16 +2,24 @@ public class RandMassiveThree {
     public static void main(String[] args) {
         int[] a = new int[99];
         int i = 0;
+        int c = 0;
 
         while (i < 99) {
             int b = (int) (Math.random() * 99);
             a[i] = b;
             i++;
         }
-        for (i = 0; i < a.length; i++) {   //в строку
-            System.out.print(a[i] + " ");
-
+        for (i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");  //в строку
         }
-       
+        System.out.println("");
+        for (i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0) {
+                c = c + 1;
+                System.out.print(a[i] + " ");
+            }
+        }
+        System.out.println("");
+        System.out.println("Count: "+ c);
     }
 }
